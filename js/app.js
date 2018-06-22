34f7e7ca3f08296e8b4e699c8e447ca3136da882
Jayce <callrj5@gmail.com>
2018-06-22 17:10:41 -0600

    activated the remove btn, add to cart button puts the class into the cart

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 // Variables
-const courses = document.querySelector('#courses-list');
+const courses = document.querySelector('#courses-list'),
+    shoppingCartContent = document.querySelector('#cart-content tbody');
 
 
 // Listeners
@@ -9,6 +10,10 @@ loadEventListeners();
 function loadEventListeners() {
     // when a new course is added
     courses.addEventListener('click', buyCourse);
+
+    // activate remove button
+
+    shoppingCartContent.addEventListener('click', removeCourse);
 }
 
 
@@ -25,5 +30,45 @@ function buyCourse(e) {
 }
 
 function getCourseInfo(course) {
-    console.log(course);
+    //create object w/ course data
+    const courseInfo = {
+        image: course.querySelector('img').src,
+        title: course.querySelector('h4').textContent,
+        price: course.querySelector('.price span').textContent,
+        id: course.querySelector('a').getAttribute('data-id')
+    }
+
+    // insert into cart
+    addIntoCart(courseInfo);
+}
+
+function addIntoCart(course) {
+
+    const row = document.createElement('tr');
+
+    row.innerHTML = ` 
+        <tr>
+            <td>
+                <img src="${course.image}">
+        </td> 
+        <td>${course.title}</td>
+         <td>${course.price}</td> 
+         <td>
+                <a href="#" class="remove" data-id="${course.id}">X</a>
+        </td>
+        
+        
+         </tr>
+    `;
+        // add into cart
+    shoppingCartContent.appendChild(row);
+}
+
+//activate remove button
+
+function removeCourse(e) {
+
+    if(e.target.classList.contains('remove')) {
+        e.target.parentElement.parentElement.remove();
+    }
 }
