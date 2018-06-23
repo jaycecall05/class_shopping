70030d0875e6ce990f7568d782fe06f9bc5b71fc
Jayce <callrj5@gmail.com>
2018-06-22 22:38:06 -0600

    saving courses to local storage

--- js/app.js
+++ js/app.js
@@ -19,6 +19,9 @@ function loadEventListeners() {
     // clear cart btn
 
     clearCartBtn.addEventListener('click', clearCart);
+
+    // document Ready
+    document.addEventListener('DOMContentLoaded', getCoursesFromStorage);
 }
 
 
@@ -27,7 +30,7 @@ function loadEventListeners() {
 function buyCourse(e) {
     e.preventDefault();
 
-    if (e.target.classList.contains('add-to-cart')) {
+    if(e.target.classList.contains('add-to-cart')) {
         const course = e.target.parentElement.parentElement;
 
         getCourseInfo(course);
@@ -67,6 +70,33 @@ function addIntoCart(course) {
     `;
         // add into cart
     shoppingCartContent.appendChild(row);
+
+    // add course into storage
+    saveIntoStorage(course);
+}
+
+// add the courses into storage
+function saveIntoStorage(course) {
+    let courses = getCoursesFromStorage();
+
+    // add the course into the array
+    courses.push(course);
+
+    localStorage.setItem('courses', JSON.stringify(courses) );
+
+}
+
+// get the contents from storage
+function getCoursesFromStorage() {
+    
+    let courses;
+    
+    if(localStorage.getItem('courses') === null) {
+        courses = [];
+    }else {
+        courses = JSON.parse(localStorage.getItem('courses') );
+    }
+    return courses;
 }
 
 //activate remove button
@@ -80,4 +110,29 @@ function removeCourse(e) {
 // clears cart
 function clearCart() {
     shoppingCartContent.innerHTML = '';
+}
+
+// loads when doc is ready and print courses to cart
+
+function getCoursesFromStorage() {
+    let coursesLS = getCoursesFromStorage();
+
+    // loop throught the courses and print in cart
+    coursesLS.forEach(function(course) {
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+            <tr>
+                <td>
+                    <img src=${course.image}" width=100>
+                </td>
+                <td>${course.title}</td>
+                <td>${course.price}</td>
+                <td>
+                    <a href="#" class="remove" data-id="${course.id}">X</a>
+                </td>
+            </tr>
+        
+        `;
+    });
 }
